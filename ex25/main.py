0e6bb18cd18926ff3f8915b5996c7acadc3643d9
Wojciech Sęk <wojciech.sek17@gmail.com>
2023-06-17 19:29:23 +0200

    added experiments

--- ex25/main.py
+++ ex25/main.py
@@ -1,4 +1,6 @@
 import random
+import matplotlib.pyplot as plt
+import scipy.stats
 
 class Bucket: # Vitter with one place
     def __init__(self):
@@ -22,34 +24,63 @@ class SingleWindow:
         self.WindowSize = window_size
 
     def onGet(self, z):
-        if self.Active is None: # Case when we are considering first n elements
-            self.Partial.onGet(z)  
-        else:
-            if self.Partial.Seen == self.WindowSize: # Partial becomes active, new partial is created
-                self.Active  = self.Partial
-                self.Partial = Bucket()
-            self.Partial.onGet(z)
-
+        self.Partial.onGet(z)  
+        if self.Partial.Seen == self.WindowSize: # Partial becomes active, new partial is created
+            self.Active  = self.Partial
+            self.Partial = Bucket()
+            
     def readData(self):
         if self.Active is None: # Case when we are considering first n elements
-            return self.Partial.readData()  
+            (_, partial_val) = self.Partial.readData()
+            return partial_val
         else:
             (active_idx, active_val) = self.Active.readData()
             (_, partial_val)         = self.Partial.readData()
             
-            is_expired = active_idx <= self.Partial.Seen
-            if is_expired:
+            if active_idx <= self.Partial.Seen:
                 return partial_val
             else:
                 return active_val
+            
+    def readIdx(self):
+        if self.Active is None: # Case when we are considering first n elements
+            (partial_idx, _) = self.Partial.readData()
+            return partial_idx - 1
+        else:
+            (active_idx, _) = self.Active.readData()
+            return (self.WindowSize + active_idx - 1 - self.Partial.Seen) % self.WindowSize
 
-class SlidingWindows:
-    def __init__(self, window_size, sample_size):
-        self.Windows    = [ SingleWindow(window_size) for _ in range(sample_size) ]
+def histogram_test():
+    window_size = 5
+    num_of_tests = 1000
+    stream_len = 1000
 
-    def onGet(self, z):
-        for window in self.Windows:
-            window.onGet(z)
+    sample = [] 
+    for _ in range(num_of_tests):
+        window = SingleWindow(window_size)
+        for i in range(stream_len):
+            window.onGet(i)
+            sample.append(window.readData())
 
-    def readData(self):
-        return [ window.readData() for window in self.Windows ]
+    plt.hist(sample, density=True, bins=stream_len)
+    plt.xlabel('Value')
+    plt.ylabel('Probability')
+    plt.title('SW distr (window size = 5, stream length = 1000)))')
+    plt.savefig('data/histogram.png', dpi=300)
+
+def chi_square_test():
+    window_size  = 5
+    stream_len   = 10000 
+    frequencies  = [0] * 5
+
+    window = SingleWindow(window_size)
+    for i in range(stream_len):
+        window.onGet(i)
+        frequencies[window.readIdx()] += 1
+
+    (chisq, p) = scipy.stats.chisquare(frequencies)
+    print(f"Chi-square statistic: {chisq}, p-value: {p}")
+
+if __name__ == '__main__':
+    histogram_test()
+    chi_square_test()
